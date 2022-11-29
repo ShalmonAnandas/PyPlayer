b9ce41a1cc887cfccbb583d0974cc2ed7228c1c0
ShalmonAnandas <nashalanandas@gmail.com>
2022-11-29 11:42:58 +0530

    migrated all statements to use fstring

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 import webbrowser
 import os
 
-os.system("cls")
+# os.system("cls")
 media_selector = int(input("Do you wanna watch a Movie or a Show?\nMovie [1]\nShow [2]\nEnter a number: "))
-os.system("cls")
+# os.system("cls")
 if media_selector == 1:
     media_name = input("Enter the name of the Movie: ")
 elif media_selector == 2:
@@ -12,7 +12,8 @@ elif media_selector == 2:
 media_query = media_name.replace(" ","+")
 media_type_dict = {1 : "movie", 2 : "tv"}
 
-response = requests.get("https://api.themoviedb.org/3/search/" + media_type_dict.get(media_selector) + "?api_key=87d5585a5497b373679e8bdc7d6f0d22&query=" + media_query + "\"")
+response = requests.get(f"https://api.themoviedb.org/3/search/{media_type_dict.get(media_selector)}?api_key=87d5585a5497b373679e8bdc7d6f0d22&query={media_query}\"")
+print(media_type_dict.get(media_selector))
 #print("https://api.themoviedb.org/3/search/" + media_type_dict.get(media_selector) + "?api_key=87d5585a5497b373679e8bdc7d6f0d22&query=" + media_query + "\"")
 request = response.json()
 result_list = request.get("results") 
@@ -22,31 +23,31 @@ id_list = []
 for i in result_list:
     counter = counter + 1
 
-os.system("cls")
+# os.system("cls")
 
 if media_selector == 1:
     for i in range(counter):
         print("-----------------------------------------------------------------------------------------------------------------------")
         result_first = result_list[i]
-        print(str(result_first.get('original_title')) + " ["+str(i)+']\n' + str(result_first.get('overview'))+ "\n") #+ str(result_first.get('id')))
+        print(f"{str(result_first.get('original_title'))}[{str(i)}]\n{str(result_first.get('overview'))}\n") #+ str(result_first.get('id')))
         id_list.append(str(result_first.get('id')))
         print("-----------------------------------------------------------------------------------------------------------------------")
 elif media_selector == 2:
     for i in range(counter):
         print("-----------------------------------------------------------------------------------------------------------------------")
         result_first = result_list[i]
-        print(str(result_first.get('name')) + " ["+str(i)+']\n' + str(result_first.get('overview')) + "\n") #+ str(result_first.get('id')))
+        print(f"{str(result_first.get('name'))}[{str(i)}]\n{str(result_first.get('overview'))}\n") #+ str(result_first.get('id')))
         id_list.append(str(result_first.get('id')))
         print("-----------------------------------------------------------------------------------------------------------------------")
 
 if media_selector == 1:
     play = int(input("\nSelect a movie [type number]: "))
     print(id_list[play])
-    webbrowser.open("https://vidsrc.me/embed/" + str(id_list[play]))
+    webbrowser.open(f"https://vidsrc.me/embed/{str(id_list[play])}")
 elif media_selector == 2:
     play = int(input("\nSelect a Show [type number]: "))
-    os.system("cls")
+    # os.system("cls")
     season_number = input("Season Number: ")
-    os.system("cls")
+    # os.system("cls")
     episode_number = input("Episode Number: ")
-    webbrowser.open("https://vidsec.me/embed/" + str(id_list[play]) + "-" + season_number + "-" + episode_number)
+    webbrowser.open(f"https://vidsrc.me/embed/{str(id_list[play])}/{season_number}-{episode_number}")
